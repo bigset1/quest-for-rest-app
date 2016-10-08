34c5271f9d152a5d8294b94f655c4bd491ad7df0
Viktor Vetsko <viktor.vetsko@gmail.com>
2016-10-08 14:55:56 +0300

    app min, max width

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,10 @@ import Paper from 'material-ui/Paper';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-const style = {width: "100%",
-               display: "inline-block"};
+const style = {
+  width: "100%",
+  display: "inline-block"
+};
 
 const paper = {
   width: "45%",
@@ -17,108 +19,107 @@ export default class Home extends React.Component {
 
   render() {
 
-    return(
+    return (
       <div>
-        <Paper style={paper} zDepth={1} >
-        <Card style={style} >
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="http://placekitten.com/200/300"
+        <Paper style={paper} zDepth={1}>
+          <Card style={style}>
+            <CardHeader
+              title="URL Avatar"
+              subtitle="Subtitle"
+              avatar="http://placekitten.com/200/300"
             />
-          <CardMedia
-            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-            >
-            <img src="http://placekitten.com/200/300" />
-          </CardMedia>
-          <CardTitle title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
+            <CardMedia overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}>
+              <img src="http://placekitten.com/200/300"/>
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle"/>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1"/>
+              <FlatButton label="Action2"/>
+            </CardActions>
+          </Card>
         </Paper>
         <Paper style={paper} zDepth={1}>
-        <Card style={style}>
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="http://placekitten.com/200/300"
+          <Card style={style}>
+            <CardHeader
+              title="URL Avatar"
+              subtitle="Subtitle"
+              avatar="http://placekitten.com/200/300"
             />
-          <CardMedia
-            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}
             >
-            <img src="http://placekitten.com/200/300" />
-          </CardMedia>
-          <CardTitle title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
+              <img src="http://placekitten.com/200/300"/>
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle"/>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1"/>
+              <FlatButton label="Action2"/>
+            </CardActions>
+          </Card>
         </Paper>
-        <Paper style={paper} zDepth={1} >
-        <Card  style={style}  >
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="http://placekitten.com/200/300"
+        d
+        <Paper style={paper} zDepth={1}>
+          <Card style={style}>
+            <CardHeader
+              title="URL Avatar"
+              subtitle="Subtitle"
+              avatar="http://placekitten.com/200/300"
             />
-          <CardMedia
-            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}
             >
-            <img src="http://placekitten.com/200/300" />
-          </CardMedia>
-          <CardTitle title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
-          </Paper>
-        <Paper style={paper} zDepth={1} >
-        <Card style={style}>
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="http://placekitten.com/200/300"
+              <img src="http://placekitten.com/200/300"/>
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle"/>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1"/>
+              <FlatButton label="Action2"/>
+            </CardActions>
+          </Card>
+        </Paper>
+        <Paper style={paper} zDepth={1}>
+          <Card style={style}>
+            <CardHeader
+              title="URL Avatar"
+              subtitle="Subtitle"
+              avatar="http://placekitten.com/200/300"
             />
-          <CardMedia
-            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}
             >
-            <img src="http://placekitten.com/200/300" />
-          </CardMedia>
-          <CardTitle title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
-          </Paper>
+              <img src="http://placekitten.com/200/300"/>
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle"/>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1"/>
+              <FlatButton label="Action2"/>
+            </CardActions>
+          </Card>
+        </Paper>
       </div>
     )
   }
